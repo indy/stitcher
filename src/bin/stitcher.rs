ab635c2a53bdea2e68e80a5794525cfeef68ca55
Inderjit Gill <email@indy.io>
2018-03-17 12:21:39 +0000

    initial commit

---
+++ src/bin/stitcher.rs
@@ -0,0 +1,36 @@
+extern crate env_logger;
+extern crate stitcher;
+extern crate clap;
+
+use clap::{Arg, App};
+
+fn main() {
+    match run() {
+        Ok(_) => (),
+        Err(err) => println!("Error: {}", err),
+    }
+}
+
+// building and running with info log levels
+// STITCHER_LOG='info' cargo run --bin stitcher -- --base something
+
+fn run() -> stitcher::Result<()> {
+    env_logger::init_from_env("STITCHER_LOG");
+
+    let matches = App::new("Stitcher")
+                          .version("0.1.0")
+                          .author("Inderjit Gill <email@indy.io>")
+                          .about("Stitches 4 images into 1")
+                          .arg(Arg::with_name("base")
+                               .short("b")
+                               .long("base")
+                               .value_name("FILE")
+                               .help("base name of input files")
+                               .required(true))
+                          .get_matches();
+
+    match matches.value_of("base") {
+        Some(base) => stitcher::stitch(base),
+        None => Err(stitcher::StitcherError::CommandLineParsingError),
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,110 @@
+#[macro_use]
+extern crate failure;
+#[macro_use]
+extern crate log;
+extern crate image;
+
+use std::io::Error as IoError;
+use std::fs::File;
+
+use image::{GenericImage, DynamicImage, ImageBuffer};
+
+/// A specialized `Result` type for the `Stitcher` crate.
+pub type Result<T> = ::std::result::Result<T, StitcherError>;
+
+#[derive(Debug, Fail)]
+pub enum StitcherError {
+    #[fail(display = "an io error occurred")]
+    Io(#[cause] IoError),
+
+    #[fail(display = "an image error occurred")]
+    ImageError(#[cause] image::ImageError),
+
+    #[fail(display = "Command line parsing")]
+    CommandLineParsingError,
+
+    #[fail(display = "Image size mismatch")]
+    SizeMismatch,
+
+    /// This allows you to produce any `failure::Error` within closures used by
+    /// the skeleton crate. No errors of this kind will ever be produced by the
+    /// crate itself.
+    #[fail(display = "{}", inner)]
+    Custom {
+        /// The actual error that occurred.
+        inner: failure::Error,
+    },
+}
+
+impl From<IoError> for StitcherError {
+    fn from(e: IoError) -> StitcherError {
+        StitcherError::Io(e)
+    }
+}
+
+impl From<image::ImageError> for StitcherError {
+    fn from(e: image::ImageError) -> StitcherError {
+        StitcherError::ImageError(e)
+    }
+}
+
+
+fn check_size(img: &DynamicImage, expected_width: u32, expected_height: u32) -> Result<()> {
+    let (width, height) = img.dimensions();
+
+    if width != expected_width || height != expected_height {
+        return Err(StitcherError::SizeMismatch);
+    }
+
+    Ok(())
+}
+
+fn copy_into(img: &mut image::RgbaImage, src: &DynamicImage, x: u32, y: u32, width: u32, height: u32) -> Result<()> {
+
+    let mut sub = img.sub_image(x, y, width, height);
+    sub.copy_from(src, 0, 0);
+
+    Ok(())
+}
+
+pub fn stitch(base: &str) -> Result<()> {
+    info!("combine: base:{}", base);
+
+    let filename_tl = format!("{}-tl.png", base);
+    let filename_tr = format!("{}-tr.png", base);
+    let filename_bl = format!("{}-bl.png", base);
+    let filename_br = format!("{}-br.png", base);
+
+    let img_tl = image::open(filename_tl)?;
+    let img_tr = image::open(filename_tr)?;
+    let img_bl = image::open(filename_bl)?;
+    let img_br = image::open(filename_br)?;
+
+    // all images should have the same dimensions
+    let (width, height) = img_tl.dimensions();
+    check_size(&img_tr, width, height)?;
+    check_size(&img_bl, width, height)?;
+    check_size(&img_br, width, height)?;
+
+    // Construct a new ImageBuffer for all 4 images
+    let mut img = ImageBuffer::new(width * 2, height * 2);
+
+    copy_into(&mut img, &img_tl, 0,     0,      width, height)?;
+    copy_into(&mut img, &img_tr, width, 0,      width, height)?;
+    copy_into(&mut img, &img_bl, 0,     height, width, height)?;
+    copy_into(&mut img, &img_br, width, height, width, height)?;
+
+    let filename_output = format!("{}-out.png", base);
+    let ref mut fout = File::create(filename_output)?;
+    image::ImageRgba8(img).save(fout, image::PNG)?;
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
