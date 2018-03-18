9ba8613591c2d8aa83ab0b1563731045f92b235d
Inderjit Gill <email@indy.io>
2018-03-18 12:34:48 +0000

    docs and fmt

--- src/bin/stitcher.rs
+++ src/bin/stitcher.rs
@@ -1,8 +1,8 @@
+extern crate clap;
 extern crate env_logger;
 extern crate stitcher;
-extern crate clap;
 
-use clap::{Arg, App};
+use clap::{App, Arg};
 
 fn main() {
     match run() {
@@ -18,16 +18,18 @@ fn run() -> stitcher::Result<()> {
     env_logger::init_from_env("STITCHER_LOG");
 
     let matches = App::new("Stitcher")
-                          .version("0.1.0")
-                          .author("Inderjit Gill <email@indy.io>")
-                          .about("Stitches 4 images into 1")
-                          .arg(Arg::with_name("base")
-                               .short("b")
-                               .long("base")
-                               .value_name("FILE")
-                               .help("base name of input files")
-                               .required(true))
-                          .get_matches();
+        .version("0.1.0")
+        .author("Inderjit Gill <email@indy.io>")
+        .about("Stitches 4 images into 1")
+        .arg(
+            Arg::with_name("base")
+                .short("b")
+                .long("base")
+                .value_name("FILE")
+                .help("base name of input files")
+                .required(true),
+        )
+        .get_matches();
 
     match matches.value_of("base") {
         Some(base) => stitcher::stitch(base),
--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,26 @@
 #[macro_use]
 extern crate failure;
+extern crate image;
 #[macro_use]
 extern crate log;
-extern crate image;
 
 use std::io::Error as IoError;
 use std::fs::File;
 
-use image::{GenericImage, DynamicImage, ImageBuffer};
+use image::{DynamicImage, GenericImage, ImageBuffer};
 
 /// A specialized `Result` type for the `Stitcher` crate.
 pub type Result<T> = ::std::result::Result<T, StitcherError>;
 
 #[derive(Debug, Fail)]
 pub enum StitcherError {
-    #[fail(display = "an io error occurred")]
-    Io(#[cause] IoError),
+    #[fail(display = "an io error occurred")] Io(#[cause] IoError),
 
-    #[fail(display = "an image error occurred")]
-    ImageError(#[cause] image::ImageError),
+    #[fail(display = "an image error occurred")] ImageError(#[cause] image::ImageError),
 
-    #[fail(display = "Command line parsing")]
-    CommandLineParsingError,
+    #[fail(display = "Command line parsing")] CommandLineParsingError,
 
-    #[fail(display = "Image size mismatch")]
-    SizeMismatch,
+    #[fail(display = "Image size mismatch")] SizeMismatch,
 
     /// This allows you to produce any `failure::Error` within closures used by
     /// the skeleton crate. No errors of this kind will ever be produced by the
@@ -48,7 +44,6 @@ impl From<image::ImageError> for StitcherError {
     }
 }
 
-
 fn check_size(img: &DynamicImage, expected_width: u32, expected_height: u32) -> Result<()> {
     let (width, height) = img.dimensions();
 
@@ -59,14 +54,39 @@ fn check_size(img: &DynamicImage, expected_width: u32, expected_height: u32) -> 
     Ok(())
 }
 
-fn copy_into(img: &mut image::RgbaImage, src: &DynamicImage, x: u32, y: u32, width: u32, height: u32) -> Result<()> {
-
+fn copy_into(
+    img: &mut image::RgbaImage,
+    src: &DynamicImage,
+    x: u32,
+    y: u32,
+    width: u32,
+    height: u32,
+) -> Result<()> {
     let mut sub = img.sub_image(x, y, width, height);
     sub.copy_from(src, 0, 0);
 
     Ok(())
 }
 
+/// Stitch together four images that meet the following requirements:
+///
+/// 1. Must be in png format
+/// 2. Have names ending in '-tl', '-tr', '-bl' and '-br' for top-left, top-right,
+///    bottom-left and bottom-right respectively
+/// 3. All images must have the same dimensions
+///
+/// # Examples
+///
+/// ```
+/// use stitcher::stitch;
+///
+/// stitch("artwork")?;
+/// ```
+///
+/// Assuming that the image files: 'artwork-tl.png', 'artwork-tl.png',
+/// 'artwork-tl.png' and 'artwork-tl.png' exist, the function will combine them
+/// into a single file called 'artwork.out' that's saved in the same location
+/// as the input files
 pub fn stitch(base: &str) -> Result<()> {
     info!("combine: base:{}", base);
 
@@ -89,9 +109,9 @@ pub fn stitch(base: &str) -> Result<()> {
     // Construct a new ImageBuffer for all 4 images
     let mut img = ImageBuffer::new(width * 2, height * 2);
 
-    copy_into(&mut img, &img_tl, 0,     0,      width, height)?;
-    copy_into(&mut img, &img_tr, width, 0,      width, height)?;
-    copy_into(&mut img, &img_bl, 0,     height, width, height)?;
+    copy_into(&mut img, &img_tl, 0, 0, width, height)?;
+    copy_into(&mut img, &img_tr, width, 0, width, height)?;
+    copy_into(&mut img, &img_bl, 0, height, width, height)?;
     copy_into(&mut img, &img_br, width, height, width, height)?;
 
     let filename_output = format!("{}-out.png", base);
